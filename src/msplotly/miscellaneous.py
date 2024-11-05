a983f47cd8d6716058b207ceb5e107568f7714f0
Ivan Munoz-Gutierrez <ivan.munoz.gutierrez@gmail.com>
2024-11-04 17:15:39 -0800

    refactored app layout

--- src/msplotly/miscellaneous.py
+++ src/msplotly/miscellaneous.py
@@ -1,4 +1,11 @@
-"""Miscellaneous functions."""
+"""Miscellaneous functions.
+
+License
+-------
+This file is part of MSPlotly
+BSD 3-Clause License
+Copyright (c) 2024, Ivan Munoz Gutierrez
+"""
 
 import importlib.resources as resources
 from pathlib import Path
@@ -7,3 +14,7 @@ from pathlib import Path
 def get_package_path(package: str = "msplotly") -> Path:
     """Get path to package directory in a src-layout"""
     return resources.files(f"{package}")
+
+
+if __name__ == "__main__":
+    print(get_package_path())
